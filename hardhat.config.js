64754cf414771d2af27a9c969b1fc23484ad65d6
fbslo <admin@fbslo.net>
2024-11-23 16:53:08 +0100

    start workling on data provider

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ const mnemonic = process.env.MNEMONIC;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.20",
+    solidity: "0.8.24",
     networks: {
         hardhat: {
             gas: "auto",
--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,9 @@
 main()
 
 async function main(){
-    const [owner] = await ethers.getSigners();
+    const [address1, address2, address3] = await ethers.getSigners();
+
+    const owner = address3;
 
     const swapper = "0x85aA63EB2ab9BaAA74eAd7e7f82A571d74901853" //hyperswap
     const pool = "0x1e85CCDf0D098a9f55b82F3E35013Eda235C8BD8" //hyperevm testnet deployment
@@ -12,20 +14,22 @@ async function main(){
         [swapper],
         owner.address
     );
+    // const looping = await ethers.getContractAt("Looping", "0xa1625001247DD1525a98c4cDb3dD9Ec1f55F1abd")
     console.log(`looping deployed to ${looping.target}`)
 
     const debtAsset = "0xe0bdd7e8b7bf5b15dcDA6103FCbBA82a460ae2C7" //WETH
     const yieldAsset = "0x453b63484b11bbF0b61fC7E854f8DAC7bdE7d458" //mBTC
     const debtAssetVariableDebtToken = "0xE5C5E18723991AF5D2a640f6C9667D48741429E6" //WETHVariableDebt
-    const initialAmount = (0.2 * Math.pow(10, 18)).toString() //0.1 ETH`
-    const flashloanAmount = (0.4 * Math.pow(10, 18)).toString() //0.3 ETH
+    const initialAmount = (0.09 * Math.pow(10, 18)).toString() //ETH
+    const flashloanAmount = (0.3 * Math.pow(10, 18)).toString() //ETH
     const minAmountOut = 0;
 
     const debtInstance = await ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", debtAsset)
     const debtVariableDebtInstance = await ethers.getContractAt("IDebtToken", debtAssetVariableDebtToken)
 
     await debtVariableDebtInstance.connect(owner).approveDelegation(looping.target, "9999999999999999999999999999999999999999999999999999999999999999999999999999")
-    await debtInstance.approve(looping.target, initialAmount)
+    await debtInstance.connect(owner).approve(looping.target, initialAmount)
+    console.log(`approved tokens`)
 
     console.log(await looping.connect(owner).leverage(
         pool,
