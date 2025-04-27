ff5f4a4a0c85e67d1c3d37b9a113f78078e44801
fbslo <admin@fbslo.net>
2025-04-27 13:28:04 +0200

    add cleanOutTokens functon to LoopingManager

--- contracts/StrategyManager.sol
+++ contracts/StrategyManager.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title StrategyManager
 /// @author HyperLend
 /// @notice contract used to manage custom strategy on behalf of the user
 contract StrategyManager is Ownable {
+    using SafeERC20 for IERC20;
+
     /// @notice variables are used on the UI to identify which StrategyManager is used for certain pairs of assets
     address immutable public pool;
     address immutable public yieldAsset;
@@ -41,6 +45,18 @@ contract StrategyManager is Ownable {
     function executeMultiCall(Call[] memory calls) external payable onlyOwner() {
         for (uint256 i  = 0; i < calls.length; i++){
             executeCall(calls[i].target, calls[i].value, calls[i].data, calls[i].allowRevert);
+        }
+    }
+
+    function cleanOutTokens(address[] memory tokens) external onlyOwner() {
+        for (uint256 i = 0; i < tokens.length; i++){
+            if (tokens[i] == address(0)){
+                    (bool sent,) = owner().call{value: address(this).balance}("");
+                    require(sent, "cleanOutTokens: failed to send native");
+            } else {
+                uint256 balance = IERC20(tokens[i]).balanceOf(address(this));
+                IERC20(tokens[i]).transfer(owner(), balance);
+            }
         }
     }
 
